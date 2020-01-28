b180e5cf000f5a5ee82f4ba674f8a40fae5205e9
GPiter <ganinpeter@gmail.com>
2020-01-29 01:12:09 +0300

    Добавлено задание 6-5. Составлены регулярные выражения

---
+++ Level_1/Lesson_6/Hw 6_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Hw_6_5
+{
+    class Program
+    {
+        // [a-zA-Z0-9]+[.](jpeg|bmp)
+        // [<]img src=["]\D[a-zA-Z0-9]*[.](jpeg|bmp)["][>]
+
+        static void Main(string[] args)
+        {
+            string pattern_teg = "[<]img src=[" + "\"" + "]\\D[a - zA - Z0 - 9]*[.] (jpeg|bmp)[" + "\"" + "][>]";
+            List<string> tegs = new List<string>();
+
+            string[] catalogs = Directory.GetFiles("D:\\Projects\\Shop\\", "*.*", SearchOption.AllDirectories);
+            Regex reg_teg = new Regex(pattern_teg);
+
+            foreach(var file in catalogs)
+            {
+                string text = File.ReadAllText(file);
+            }
+        }
+    }
+}
