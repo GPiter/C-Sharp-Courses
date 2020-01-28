9e41b1ab4a85f7679e9efa6b81f0b9a5769c6f44
GPiter <GaninPeter@gmail.com>
2020-01-28 19:11:13 +0300

    Добавлено задание 6-4. Поиск телефонов в файлах и директориях по регулярному выражению

---
+++ Level_1/Lesson_6/Hw 6_4/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задание 6-4
+ * В файле могут встречаться номера телефонов, записанные в формате
+ * xx-xx-xx, xxx-xxx или xxx-xx-xx.
+ * Вывести все номера телефонов, которые содержатся в файле.
+ */
+
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Hw_6_4
+{
+    // Составленное регулярное выражение для поиска телефонов:
+    // (\d{3}(-\d\d){2}) | ((\d\d-){2}\d\d) | (\d{3}-\d{3})
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fs = Directory.GetFiles("D:\\test", "*.*", SearchOption.AllDirectories);   // Создаем массив, хранящий список всех директорий, включая подпапки
+
+            foreach(var filename in fs)
+            {
+                Regex regex = new Regex(@"(\d{3}(-\d\d){2})|((\d\d-){2}\d\d)|(\d{3}-\d{3})");   // Создаем регулярное выражение по составленному шаблону
+                string text = File.ReadAllText(filename);   // Считываем весь текст в текущем файле из массива
+
+                foreach (var phone in regex.Matches(text))  // Находим соответсвия телефонов в тексте файла
+                {
+                    FileInfo description = new FileInfo(filename);  //  Создаем объект для описание файла с найденным телефоном
+                    Console.WriteLine("Найден телефон " + phone + " в файле " + description.FullName);
+                }
+                    
+            }
+        }
+    }
+}
