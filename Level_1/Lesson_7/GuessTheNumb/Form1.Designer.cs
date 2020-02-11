304639a511f54afd38ec4ea17b6c8d4b43d260f6
GPiter <GaninPeter@gmail.com>
2020-02-11 18:18:27 +0300

    Добавлено задание 7-2. Игра Угадай число с использование Windows Forms

---
+++ Level_1/Lesson_7/GuessTheNumb/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GuessTheNumb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNumb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbNumb
+            // 
+            this.tbNumb.Location = new System.Drawing.Point(100, 50);
+            this.tbNumb.Name = "tbNumb";
+            this.tbNumb.Size = new System.Drawing.Size(100, 20);
+            this.tbNumb.TabIndex = 0;
+            this.tbNumb.TextChanged += new System.EventHandler(this.TbNumb_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(111, 95);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(86, 170);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(124, 13);
+            this.lblCount.TabIndex = 4;
+            this.lblCount.Text = "Количество попыток: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 362);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNumb);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNumb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ Level_1/Lesson_7/GuessTheNumb/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GuessTheNumb
+{
+    public partial class Form1 : Form
+    {
+        // ------ ОПИСАНИЕ ПОЛЕЙ ------
+
+        int input_numb;
+        int guess_numb;
+        int count = 0;
+
+        // ------ ОПИСАНИЕ КОНСТРУКТОРА ------
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen; // Установка позиции формы по центру
+
+            Random rnd = new Random();
+            guess_numb = rnd.Next(1, 101);  // Задаем число, которое нужно отгадать, в диапазоне от 1 до 100
+        }
+
+        // ----- ОПИСАНИЕ МЕТОДОВ -----
+
+        // Метод при событии нажатия на копку "Отправить"
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            count++;    // Увеличиваем счетчик - число попыток
+            lblCount.Text = "Количество попыток: " + count.ToString();   // Обновляем значение счетчика на экране
+            CheckNumb();    // Проверяем на соответсвие введенное число
+        }
+
+        // Метод для считывания введенного числа
+        private void TbNumb_TextChanged(object sender, EventArgs e)
+        {
+            input_numb = int.Parse(tbNumb.Text);
+        }
+
+        // Метод проверки веденного числа
+        private void CheckNumb()
+        {
+            if(input_numb == guess_numb)
+            {
+                MessageBox.Show("Вы угадали число! Поздравляем с победой! \nКоличество попыток: " + count.ToString());
+                Application.Exit();
+            }
+
+            if(input_numb > guess_numb) MessageBox.Show("Ваше число больше загаданного");
+            else MessageBox.Show("Ваше число меньше загаданного");
+        }
+    }
+}
