dcce3c5facf65e4697bd4812f7345a31f2bca73d
GPiter <GaninPeter@gmail.com>
2020-02-12 19:24:20 +0300

    Добавлено задание 7-3. Генератор заявления на отпуск. Создана форма, добавлены основные элементы управления

---
+++ Level_1/Lesson_7/ApplicationGenerator/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Template_rest_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbName1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbName2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbName4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbName3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbDate3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtbDate2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtbDate1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtbName5 = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtbName1
+            // 
+            this.txtbName1.Location = new System.Drawing.Point(165, 41);
+            this.txtbName1.Name = "txtbName1";
+            this.txtbName1.Size = new System.Drawing.Size(117, 20);
+            this.txtbName1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя организации";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ФИО (кому)";
+            // 
+            // txtbName2
+            // 
+            this.txtbName2.Location = new System.Drawing.Point(165, 75);
+            this.txtbName2.Name = "txtbName2";
+            this.txtbName2.Size = new System.Drawing.Size(117, 20);
+            this.txtbName2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ФИО (от кого)";
+            // 
+            // txtbName4
+            // 
+            this.txtbName4.Location = new System.Drawing.Point(165, 144);
+            this.txtbName4.Name = "txtbName4";
+            this.txtbName4.Size = new System.Drawing.Size(117, 20);
+            this.txtbName4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Должность (от кого)";
+            // 
+            // txtbName3
+            // 
+            this.txtbName3.Location = new System.Drawing.Point(165, 110);
+            this.txtbName3.Name = "txtbName3";
+            this.txtbName3.Size = new System.Drawing.Size(117, 20);
+            this.txtbName3.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Дата заявления";
+            // 
+            // txtbDate3
+            // 
+            this.txtbDate3.Location = new System.Drawing.Point(165, 281);
+            this.txtbDate3.Name = "txtbDate3";
+            this.txtbDate3.Size = new System.Drawing.Size(117, 20);
+            this.txtbDate3.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Дата окончания отпуска";
+            // 
+            // txtbDate2
+            // 
+            this.txtbDate2.Location = new System.Drawing.Point(165, 247);
+            this.txtbDate2.Name = "txtbDate2";
+            this.txtbDate2.Size = new System.Drawing.Size(117, 20);
+            this.txtbDate2.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Дата начала отпуска";
+            // 
+            // txtbDate1
+            // 
+            this.txtbDate1.Location = new System.Drawing.Point(165, 212);
+            this.txtbDate1.Name = "txtbDate1";
+            this.txtbDate1.Size = new System.Drawing.Size(117, 20);
+            this.txtbDate1.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 181);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "ФИО (кто)";
+            // 
+            // txtbName5
+            // 
+            this.txtbName5.Location = new System.Drawing.Point(165, 178);
+            this.txtbName5.Name = "txtbName5";
+            this.txtbName5.Size = new System.Drawing.Size(117, 20);
+            this.txtbName5.TabIndex = 8;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(76, 333);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(168, 29);
+            this.btnGenerate.TabIndex = 17;
+            this.btnGenerate.Text = "Сгенерировать завяление";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(578, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Результат";
+            // 
+            // txtbResult
+            // 
+            this.txtbResult.Location = new System.Drawing.Point(437, 31);
+            this.txtbResult.Multiline = true;
+            this.txtbResult.Name = "txtbResult";
+            this.txtbResult.Size = new System.Drawing.Size(333, 382);
+            this.txtbResult.TabIndex = 16;
+            this.txtbResult.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtbResult_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtbResult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtbDate3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtbDate2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtbDate1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtbName5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbName4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbName3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbName2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbName1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbName1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbName2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbName4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbName3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbDate3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtbDate2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtbDate1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtbName5;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtbResult;
+    }
+}
+
