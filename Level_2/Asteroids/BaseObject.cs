a0f89b448bb46867e5d7b83d25b6f0275f7505c6
GPiter <GaninPeter@gmail.com>
2020-03-04 20:17:00 +0300

    Создание класса астероид. Рефакторинг кода. Добавление картинок для объектов

--- Level_2/Asteroids/BaseObject.cs
+++ Level_2/Asteroids/BaseObject.cs
@@ -2,15 +2,19 @@
 
 namespace Asteroids
 {
-    /*----- КЛАСС BaseObject -----*/
-    // Базовый класс, который задает общие характеристики и поведение для всех объектов
-    class BaseObject
-    {
-        protected Point pos;
-        protected Point dir;
-        protected Size size;
 
-        //Image img = Image.FromFile("asteroid.bmp");
+    /* -------------- КЛАСС BaseObject --------------
+      * 
+      *  Базовый класс, который задает общие характеристики и поведение для всех объектов.
+      * 
+    */
+    abstract class BaseObject
+    {
+        // ------ ОПИСАНИЕ ПОЛЕЙ ------
+
+        protected Point pos;    // Позиция на экране
+        protected Point dir;    // Направление движения
+        protected Size size;    // Размер объекта
 
         public BaseObject(Point pos, Point dir, Size size)
         {
@@ -19,45 +23,58 @@ namespace Asteroids
             this.size = size;
         }
 
-        public virtual void Draw()
-        {
-            Game.buffer.Graphics.DrawEllipse(Pens.White, pos.X, pos.Y, size.Width, size.Height);
-            //Game.buffer.Graphics.DrawImage(img, pos.X, pos.Y);
-        }
+        // ------ ОПИСАНИЕ МЕТОДОВ ------
 
+        // Отрисовка объекта в буффер
+        public abstract void Draw();
+
+        // Обновление объекта на следующем кадре
         public virtual void Update()
         {
             pos.X += dir.X;
-            pos.Y += dir.Y;
-            if (pos.X < 0) dir.X = -dir.X;
-            if (pos.X > Game.Width) dir.X = -dir.X;
-            if (pos.Y < 0) dir.Y = -dir.Y;
-            if (pos.Y > Game.Height) dir.Y = -dir.Y;
+            if (pos.X < 0) pos.X = Game.Width + size.Width;
         }
 
+        // Переопределение метода ToString() для вывода информации об объекте
         public override string ToString()
         {
             return "Pos: " + pos.ToString() + " Dir: " + dir.ToString() + " Size: " + size.ToString(); 
         }
     }
 
-    /*----- КЛАСС Star -----*/
-    // Описывает характеристики и поведение для объекта типа "Звезда"
+    /* -------------- КЛАСС Star --------------
+      * 
+      *  Описывает характеристики и поведение для объекта типа "Звезда"
+      * 
+    */
     class Star : BaseObject
     {
+        // ------ ОПИСАНИЕ МЕТОДОВ ------
         public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
         {
 
         }
 
+        // Переопределение метода отрисовки объекта
         public override void Draw()
         {
             Game.buffer.Graphics.DrawLine(Pens.White, pos.X, pos.Y, pos.X + size.Width, pos.Y + size.Height);
             Game.buffer.Graphics.DrawLine(Pens.White, pos.X + size.Width, pos.Y, pos.X, pos.Y + size.Height);
         }
 
+        // Переопределение метода обновления объекта
         public override void Update()
         {
+            /*
+            pos.X += dir.X;
+            if (pos.X < 0)
+            {
+                pos.X = 900;
+                pos.Y = Game.rnd.Next(0, Game.Height);
+                dir.X = -Game.rnd.Next(1, 10);
+            }
+            */
+            
             pos.X += dir.X;
             if (pos.X < 0)
             {
@@ -66,12 +83,23 @@ namespace Asteroids
                 if (pos.Y > Game.Height) dir.Y = -dir.Y;
                 if (pos.Y < 0) dir.Y = -dir.Y;
             }
+            
 
         }
     }
 
+    /* -------------- КЛАСС Planet --------------
+      * 
+      *  Описывает характеристики и поведение для объекта типа "Планета"
+      * 
+    */
+
     class Planet : BaseObject
     {
+        Image img = Image.FromFile("img\\earth.bmp");
+
+        // ------ ОПИСАНИЕ МЕТОДОВ ------
+
         public Planet(Point pos, Point dir, Size size) : base(pos, dir, size)
         {
 
@@ -79,7 +107,7 @@ namespace Asteroids
 
         public override void Draw()
         {
-            Game.buffer.Graphics.FillEllipse(Brushes.Green, pos.X, pos.Y, size.Width, size.Height);
+            Game.buffer.Graphics.DrawImage(img, pos.X, pos.Y);
         }
 
         public override void Update()
@@ -87,11 +115,29 @@ namespace Asteroids
             pos.X += dir.X;
             if (pos.X < Game.Width) dir.X = -dir.X;
             if (pos.X > Game.Width/2) dir.X = -dir.X;
-            /*
-            if (pos.X < Game.Width / 2) dir.X = -dir.X;
-            if (pos.X > 0) dir.X = -dir.X;
-            */
         }
 
+    }
+
+    /* -------------- КЛАСС Asteroid --------------
+      * 
+      *  Описывает характеристики и поведение для объекта типа "Астероид"
+      * 
+    */
+    class Asteroid : BaseObject
+    {
+        public int Power { get; set; }
+
+        Image img = Image.FromFile("img\\asteroid.bmp");    // Изображение объекта
+
+        public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            Power = 1;
+        }
+
+        public override void Draw()
+        {
+            Game.buffer.Graphics.DrawImage(img, pos.X, pos.Y);
+        }
     }
 }
--- Level_2/Asteroids/Game.cs
+++ Level_2/Asteroids/Game.cs
@@ -1,23 +1,44 @@
 ﻿using System.Windows.Forms;
 using System.Drawing;
+using System;
 
 namespace Asteroids
 {
     static class Game
     {
+        // ------ ОПИСАНИЕ ПОЛЕЙ ------
+
+        // Массив базовых объектов (астероиды, звезды)
         static BaseObject[] objects;
+        static Asteroid[] asteroids;
+
+        // Объект планеты
         static Planet earth;
+
+        // Поля для создания графического буффера
         static BufferedGraphicsContext context;
         static public BufferedGraphics buffer;
 
+        // Свойства для задания ширины и высоты игрового поля
         static public int Width { get; set; }
         static public int Height { get; set; }
+
+        static public Random rnd = new Random();
 
         static Game()
         {
 
         }
 
+        // ------ ОПИСАНИЕ МЕТОДОВ ------
+
+        /* --- Инициализация формы ---
+         * Для вывода графики на форму используется класс Graphic.
+         * Для того, чтобы убрать мерцание в игре будем выводить графику в промежуточный буфер <buffer>.
+         * Для получения графического буфера используется класс BufferedGraphicsManager и его свойство Current.
+         * Для связи буфера и графики используется метод Allocate.
+         * 
+         */
         static public void Init(Form form)
         {
             Graphics gr;
@@ -27,7 +48,10 @@ namespace Asteroids
             Height = form.Height;
             buffer = context.Allocate(gr, new Rectangle(0, 0, Width, Height));
 
-            Load(); // Загружаем все объекты в игре
+            // Загрузка всех игровых объектов
+            Load(); 
+
+            // Установка таймера для связывания события отрисовки и обновления экрана
             Timer timer = new Timer();
             timer.Interval = 100;
             timer.Tick += Timer_Tick;
@@ -41,6 +65,32 @@ namespace Asteroids
             Update();
         }
 
+        // Загрузка всех игровых объектов
+        static public void Load()
+        {
+            earth = new Planet(new Point(700, 200), new Point(-2, 0), new Size(50, 50));
+            objects = new BaseObject[30];
+            asteroids = new Asteroid[3];
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                int r = rnd.Next(5, 50);
+                objects[i] = new Star(new Point( 600, Game.rnd.Next(0, Game.Height) ), new Point(-r, r), new Size(5, 5));
+            }
+
+            for (int i = 0; i < asteroids.Length; i++)
+            {
+                int r = rnd.Next(5, 50);
+                asteroids[i] = new Asteroid(new Point( 600, Game.rnd.Next(0, Game.Height) ), new Point(-r/5, r), new Size(r, r));
+            }
+
+        }
+
+        /* --- Отрисовка формы и игровых объектов ---
+         * В начале очищаем экран, закрашивая его черным цветом.
+         * Для отрисовки объектов в буффер, вызываем соответствующий метод.
+         * После того, как графический кадр сформирован, нужно вывести его на экран методом Render.
+         */
         static public void Draw()
         {
             buffer.Graphics.Clear(Color.Black);
@@ -50,31 +100,30 @@ namespace Asteroids
                 obj.Draw();
             }
 
+            foreach(BaseObject obj in asteroids)
+            {
+                obj.Draw();
+            }
+
             earth.Draw();
 
             buffer.Render();
         }
 
-        static public void Load()
-        {
-            earth = new Planet(new Point(700, 200), new Point(-2, 0), new Size(50, 50));
-
-            objects = new BaseObject[30];
-            for (int i = 0; i < objects.Length/2; i++)
-                objects[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
-            for (int i = objects.Length / 2; i < objects.Length; i++)
-                objects[i] = new Star(new Point(600, i * 20), new Point(-i, 100), new Size(5, 5));
-            
-        }
-
+        // Обновление данных игровых объектов
         static public void Update()
         {
-            foreach(BaseObject obj in objects)
+            earth.Update();
+
+            foreach (BaseObject obj in objects)
             {
                 obj.Update();
             }
-           
-            earth.Update();
+
+            foreach (BaseObject obj in asteroids)
+            {
+                obj.Update();
+            }
 
         }
     }
