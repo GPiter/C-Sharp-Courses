937aac39fdc29779d2c086d453a197228c3f7bf2
GPiter <ganinpeter@gmail.com>
2020-03-18 00:15:26 +0300

    Добавлено задание 3-1. Создание и обработка исключений

---
+++ Level_2/MyOwnException/MyOwnException/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Создайте собственное исключение MyOwnException.Реализуйте ввод данных в массив. Если
+ * при вводе данных вводится данное, которое уже есть в списке, создайте срабатывание вашего
+ * события и перехват его.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace MyOwnException
+{
+
+    class MyException : Exception
+    {
+        public MyException ()
+        {
+            Console.WriteLine(base.Message);
+        }
+    }
+
+    class Program
+    {
+        static int[] arr = new int[15];
+        static List<int> buffer = new List<int>();
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                try
+                {
+                    arr[i] = int.Parse(Console.ReadLine());
+
+                    foreach (int numb in buffer)
+                    {
+                        if (numb == arr[i]) throw new MyException();
+                    }
+
+                    buffer.Add(arr[i]);
+                }
+
+                catch (MyException)
+                {
+                    Console.WriteLine("Данное значение уже было введено!");
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неверный формат введенных данных!");
+                    continue;
+                }
+
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Введено слишком большое число!");
+                    continue;
+                }
+            }
+
+        }
+    }
+}
