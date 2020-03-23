85cf394bd57b0e4177de0108e8d7b90922d744ff
GPiter <GaninPeter@gmail.com>
2020-03-23 18:13:15 +0300

    Добавлено задание 4-3. Практика с коллекцией Stack

---
+++ Level_2/CheckBrackets/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задание 4-3
+ * С клавиатуры вводится скобочное выражение. Требуется проверить
+ * правильность расстановки скобок.(Использовать обобщенную коллекцию Stack<char>)
+ *
+ */
+using System;
+using System.Collections.Generic;
+
+namespace CheckBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> brackets = new Stack<char>();
+
+            char input = 'a';
+            char output;
+
+            Console.WriteLine("Проверка скобочного выражения \n1. Вводить скобки посимвольно \n2. Для выхода и проверки ввести 0");
+
+            while (input != '0')
+            {
+                try
+                {
+                    input = char.Parse(Console.ReadLine());
+                }
+
+                catch(FormatException)
+                {
+                    Console.WriteLine("Неверный формат входных данных! Введите символ!");
+                    continue;
+                }
+
+                if (input == '(')
+                {
+                    brackets.Push(input);
+                }
+
+                if (input == ')')
+                {
+                    if (brackets.Count == 0)
+                    {
+                        Console.WriteLine("Неверное начало выражения! Начните с )");
+                        continue;
+                    }
+
+                    if ( brackets.Peek() == '(' ) output = brackets.Pop();
+                }
+            }
+
+            if (brackets.Count == 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
---
+++ Level_2/CheckBrackets/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CheckBrackets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Win-Torrent Yagd")]
+[assembly: AssemblyProduct("CheckBrackets")]
+[assembly: AssemblyCopyright("Copyright © Win-Torrent Yagd 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("267c0a62-410a-4655-a621-cc16f72277ba")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
