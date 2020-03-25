024769c223ba35be37e182008db06bae0fa7d69d
GPiter <GaninPeter@gmail.com>
2020-03-25 18:35:04 +0300

    Добавлено задание 4-4б. Работа с лямбда-выражениями

---
+++ Level_2/ListPractice/LambdaPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * а) Сверните обращение к OrderBy с использованием лямбда выражения
+ * б) *Разверните обращение к OrderBy с использованием делегата Predicate<T>
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                { "four" , 4 },
+                { "two" , 2 },
+                { "one" , 1 },
+                { "three" , 3 },
+            };
+
+            //var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; } );
+
+            var d = dict.OrderBy(p => p.Value);
+
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}" , pair . Key , pair . Value );
+            }
+        }
+    }
+}
