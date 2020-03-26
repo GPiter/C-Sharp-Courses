24690460b3958d8cb0363662e4d279fbccd0b904
GPiter <GaninPeter@gmail.com>
2020-03-26 18:23:29 +0300

    Обновлено задание 4-2. Добавлена реализация с помощью делегата Func

--- Level_2/ListPractice/LambdaPractice/Program.cs
+++ Level_2/ListPractice/LambdaPractice/Program.cs
@@ -22,14 +22,28 @@ namespace LambdaPractice
                 { "three" , 3 },
             };
 
+            // Решение 1 - с помощью анонимного метода
             //var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; } );
 
-            var d = dict.OrderBy(p => p.Value);
+            // Решение 2 - с помощью лямбда выражения
+            //var d = dict.OrderBy(p => p.Value);
+
+            // Решение 3 - с помощью делегата Func
+            Func<KeyValuePair<string, int>, int> func = new Func<KeyValuePair<string, int>, int>(MethodInt);
+
+            var d = dict.OrderBy(func);
 
             foreach (var pair in d)
             {
                 Console.WriteLine("{0} - {1}" , pair . Key , pair . Value );
             }
+
         }
+
+        static int MethodInt(KeyValuePair<string, int> pair)
+        {
+            return pair.Value;
+        }
+
     }
 }
