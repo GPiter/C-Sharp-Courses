0c8a6f025bc80a607ecf172e0508142df94b48aa
GPiter <GaninPeter@gmail.com>
2020-04-03 19:27:54 +0300

    Добавлено приложение Ежедневник. Реализован базовый функционал

---
+++ Level_2/DailyPlanner/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyPlanner
+{
+    public partial class Form1 : Form
+    {
+        string notepadFilename;
+        public Form1()
+        {
+            InitializeComponent();
+            lblTimer.DataBindings.Add("Text", trbTimer, "Value");
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            tsslCurrentTime.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void TsbCurrentTimeInsert_Click(object sender, EventArgs e)
+        {
+            // Формируем текст в формате .rtf
+            //      \rtf1 - формат rtf
+            //      \b    - жирный шрифт
+            //      \b0   - окончание жирного шрифта
+            //      \par  - отступ параграфа
+            //      \line - отступ строки (аналог \n)
+
+            string rtfText = @"{\rtf1\b " + DateTime.Now.ToString() + @"\b0\par\line}}";
+
+            // Устанавливаем фокус ввода
+            rtbNotepad.Focus();
+
+            // Устанавливаем курсор в начало текстового поля
+            rtbNotepad.Select(0, 0);
+
+            // Копируем в буфер обмена данные с указанием типа данных
+            Clipboard.SetData(DataFormats.Rtf, rtfText);
+
+            // Вставляем данные из буфера обмена
+            rtbNotepad.Paste();
+
+            // Устанавливаем курсор для ввода данных
+            rtbNotepad.Select(rtbNotepad.SelectionStart - 1, 0);
+        }
+
+        private void TsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            rtbNotepad.SaveFile(notepadFilename);
+            Properties.Settings.Default.notepadFilename = notepadFilename;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notepadFilename = Properties.Settings.Default.notepadFilename;
+            if (System.IO.File.Exists(notepadFilename)) rtbNotepad.LoadFile(notepadFilename);
+        }
+
+        private void TsmiSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Файл rtf|*.rtf|Все файлы|*.*";
+
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                notepadFilename = dlg.FileName;
+                rtbNotepad.SaveFile(notepadFilename);
+            }
+        }
+
+        private void TsmiOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Файл rtf|*.rtf|Все файлы|*.*";
+
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                notepadFilename = dlg.FileName;
+                rtbNotepad.LoadFile(notepadFilename);
+            }
+        }
+
+        private void TsmiNew_Click(object sender, EventArgs e)
+        {
+            rtbNotepad.Clear();
+        }
+
+        private void TsmiSave_Click(object sender, EventArgs e)
+        {
+            rtbNotepad.SaveFile(notepadFilename);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = !timer2.Enabled;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (trbTimer.Value > 0) trbTimer.Value--;
+            else
+            {
+                timer2.Stop();
+                MessageBox.Show(txtbTimerMsg.Text);
+            }
+        }
+    }
+}
